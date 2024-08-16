e8faf7e389c4265df6c45f6af00e6414f6a144b5
Matheus Guerra <matheusaraujoguerra2603@outlook.com>
2024-08-15 22:29:50 -0300

    Update 1-get_post.py

--- 1-get_post.py
+++ 1-get_post.py
@@ -1,4 +1,20 @@
 import requests
 
 # API Jsonplaceholder
-url = 'https://jsonplaceholder.typicode.com/'
+url = 'https://jsonplaceholder.typicode.com/posts/5'
+
+# 2 - Requisição GET
+
+response = requests.get(url)
+print(response)
+print(response.status_code)
+
+# 3 - Exempolo de tratamento
+if response.status_code == 200:
+    print('Código de Sucesso foi retornado')
+else:
+    print('A requisição não foi processada corretamente')
+    
+# 4 - Pegar os dados
+response_json = response.json()
+print(response_json)
