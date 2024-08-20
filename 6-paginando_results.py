cc80d553261f079aec758437775016fbe5dfab30
MatheusGuerraa77 <matheusaraujoguerra2603@outlook.com>
2024-08-20 18:34:42 -0300

    Update 6-paginando_results.py

--- 6-paginando_results.py
+++ 6-paginando_results.py
@@ -7,3 +7,23 @@ headers = {
     'Authorization': 'Bearer' +acess_token,
     'X-GitHub-Api-Version': '2022-11-28'
 }
+
+# 2 - Mapeando informações
+base_api = 'https://api.github.com'
+user = 'matheusguerraa77'
+url = f'{base_api}/users/{user}/repos'
+
+# 3 - Organizando os dados
+repo_list = []
+for page_num in range (1, 3):
+    try:
+        url_page = f'{url}?page={page_num}'
+        response = requests.get(url_page, headers=headers)
+        repo_list.append(response.json())
+    except:
+        repo_list.append(None)
+        
+        
+# 4 - Apresentar os Dados
+print(len(repo_list))
+print(repo_list[0][2]['name'])
