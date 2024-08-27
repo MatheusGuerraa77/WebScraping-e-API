f15d5dac0ba8e3dcea5704b66521a5cf164b224d
Matheus Guerra <matheusaraujoguerra2603@outlook.com>
2024-08-27 16:22:37 -0300

    update index.html e 7-bsoup(coletando mais informações)

--- 7-bsoup.py
+++ 7-bsoup.py
@@ -12,5 +12,17 @@ vagas = soup.find('h5')
 # print(vagas)
 cursos = soup.find_all('h5')
 # print(cursos)
-for curso in cursos:
-    print(curso.text)
+# for curso in cursos:
+#     print(curso.text)
+
+# 3 - Pegando todas as informações
+course_cards = soup.find_all('div',  class_='card')
+# print(course_cards)
+for course in course_cards:
+    # print(course.h5.text)
+    # print(course.p.text)
+    # print(course.a.text.split()[-1])
+    name = course.h5.text
+    description = course.p.text
+    price = course.a.text.split()[-1]
+    print(f'{name} com a descrição: {description}, custa:{price} reais')
