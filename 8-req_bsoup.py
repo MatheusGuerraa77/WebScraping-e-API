bdc75582a97e469303b548733edf5498a5dcce6a
Matheus Guerra <matheusaraujoguerra2603@outlook.com>
2024-08-31 00:44:19 -0300

    Update 8-req_bsoup.py

--- 8-req_bsoup.py
+++ 8-req_bsoup.py
@@ -1,4 +1,11 @@
 from bs4 import BeautifulSoup
 import requests
 
-# Coletando vags em Python
+# 1 - Coletando vagas em Python
+response = requests.get('https://www.timesjobs.com/candidate/job-search.html?searchType=personalizedSearch&from=submit&searchTextSrc=&searchTextText=&txtKeywords=Python&txtLocation=')
+print(response.status_code)
+# print(response.text)
+soup = BeautifulSoup(response.text, 'lxml')
+jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
+print(len(jobs))
+print(jobs[:3])
