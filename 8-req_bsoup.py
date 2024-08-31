7ab01aa277a39f377b06080418fcc9c7dea48b8a
Matheus Guerra <matheusaraujoguerra2603@outlook.com>
2024-08-31 01:14:25 -0300

    Update 8-req_bsoup.py

--- 8-req_bsoup.py
+++ 8-req_bsoup.py
@@ -7,5 +7,15 @@ print(response.status_code)
 # print(response.text)
 soup = BeautifulSoup(response.text, 'lxml')
 jobs = soup.find_all('li', class_='clearfix job-bx wht-shd-bx')
-print(len(jobs))
-print(jobs[:3])
+# print(len(jobs))
+# print(jobs[:3])
+
+# 2 - Estruturando informações para coleta
+for job in jobs:
+    name_company = job.find('h3', class_='joblist-comp-name').text.strip().replace(' ', '')
+    # print(name_company)
+    skill = job.find('span', class_='srp-skills').text.strip().replace(' ', '')
+    # print(skill)
+    pub_date = job.find('span', class_='sim-posted').span.text[7:]
+    print(pub_date)
+    
